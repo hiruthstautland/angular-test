28436abe4bbd9cfe8921045e5888c9f1ce5481e9
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-07-31 01:50:57 +0200

    added search field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './components/first/first.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
@@ -13,9 +14,9 @@ import { TodoItemComponent } from './components/todo-item/todo-item.component';
 import { GetTeamsComponent } from './components/get-teams/get-teams.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-// In a big project, create a specific module for Material and export MatModules 
-// to be used in all modules and components of the project 
-// by importing only one module, your “MaterialModule”. 
+// In a big project, create a specific module for Material and export MatModules
+// to be used in all modules and components of the project
+// by importing only one module, your “MaterialModule”.
 // Otherwise, you have to import the MatModules every time in each module.
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
@@ -44,6 +45,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatCardModule,
     MatButtonModule,
     MatProgressSpinnerModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
