456fa790c0810abd46bf03de2eddb9cb406ed1d2
Hiruth Marie Stautland <hiruthstautland@gmail.com>
2020-08-05 23:54:46 +0200

    text are input comp

--- src/app/components/chat-field/chat-field.component.ts
+++ src/app/components/chat-field/chat-field.component.ts
@@ -3,13 +3,56 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-chat-field',
   templateUrl: './chat-field.component.html',
-  styleUrls: ['./chat-field.component.scss']
+  styleUrls: ['./chat-field.component.scss'],
 })
 export class ChatFieldComponent implements OnInit {
+  availableFriends: Array<Object>;
+  suggestions: Array<Object> = [];
+  currentRows: number = 3;
+  textInArea: string = '';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  getSuggestions() {
+    // check for match
+    this.textFunction();
   }
+  // autocomp
+  findFriend(input: string, availableSuggestions: Array<Object>): void {
+    this.suggestions = availableSuggestions.filter((sug: Object) => {
+      return (
+        sug['name'].substr(0, input.length).toLowerCase() ===
+        input.toLowerCase()
+      );
+    });
+  }
+  textFunction(): string {
+    let re = /@\w+/i;
+    let text = this.textInArea;
+    let addContact = text.match(re);
+    // detect @ with following chars
+    if (addContact) {
+      // get/fetch possible friends preff after @detected
+      this.availableFriends = this.friendsObj;
+      // get name (after @)
+      let handler = addContact[0].slice(1);
+      // check if handler matches friends
+      this.findFriend(handler, this.availableFriends);
+    }
 
+    return this.textInArea;
+  }
+  friendsObj = [
+    { name: 'Hiruth' },
+    { name: 'Cindy' },
+    { name: 'Charlize' },
+    { name: 'Cathy' },
+    { name: 'casanova' },
+    { name: 'clippers' },
+    { name: 'Zara' },
+    { name: 'Althea' },
+    { name: 'Therese' },
+    { name: 'Elise' },
+  ];
 }
